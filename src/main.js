5e4157af85040df80d5ab0dbdd0f1628e387f40b
arsize <arsizeof3416@gmail.com>
2024-06-29 16:28:10 +0800

    feat: init temple

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import { createSSRApp } from "vue"
-import * as Pinia from "pinia"
-
 import App from "./App.vue"
+import * as Pinia from "pinia"
+import piniaPersist from "pinia-plugin-persist-uni"
 
 export function createApp() {
   const app = createSSRApp(App)
-  app.use(Pinia.createPinia())
+  app.use(Pinia.createPinia().use(piniaPersist))
   return {
     app,
     Pinia,
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,41 +1,25 @@
-<template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png"></image>
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
-  </view>
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      title: 'Hello',
-    }
-  },
-  onLoad() {},
-  methods: {},
-}
+<script setup>
+import { useCounterStore } from "@/store/counter"
+const counter = useCounterStore()
+counter.count++
+counter.$patch({ count: counter.count + 1 })
+counter.increment()
 </script>
 
-<style>
+<template>
+  <div class="color">Current Counts: {{ counter.count }}</div>
+</template>
+
+<style scoped lang="scss">
+.color {
+  color: red;
+}
 .content {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
 }
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
 .text-area {
   display: flex;
   justify-content: center;
